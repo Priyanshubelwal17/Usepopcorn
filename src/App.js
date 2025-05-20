fa0be96783b52b2e9638f43fc08ecb785431d2a7
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-20 14:12:38 +0530

    Refactor layout using component composition

--- src/App.js
+++ src/App.js
@@ -54,17 +54,24 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <SearchBar />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
-function NavBar({ movies }) {
+function NavBar({ movies, children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <SearchBar />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -99,18 +106,11 @@ function SearchBar() {
   );
 }
 
-function Main({ movies }) {
-  const [query, setQuery] = useState("");
-
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -121,7 +121,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
