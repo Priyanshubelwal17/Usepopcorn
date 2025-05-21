a081ceab42f62e00fa5679eb55aad44bda9cad04
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-21 10:34:58 +0530

    Creating the stars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 
 import StarRating from "./StarRating";
@@ -10,8 +10,6 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
   </React.StrictMode>
 );
 
