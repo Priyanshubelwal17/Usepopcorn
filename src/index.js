8429ee3651b1494e4d76706062e6f491b31ac8e2
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-05-25 16:53:33 +0530

    Learn how to fetch data in react

--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
-import React, { useState } from "react";
-
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
-
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={"13"} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} stars</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={"5"}
+    <App />
+    {/* <StarRating
+      maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={33} color="red" />
-    <StarRating
-      size={70}
-      color="orangered"
-      className="test"
-      defaultRating={3}
-      messages={["no", "not bad", "good", "wow", "excellent"]}
-    />
-    <Test />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
