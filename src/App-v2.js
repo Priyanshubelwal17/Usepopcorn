e3ced3337ad3f286bbb264bdfe10b79150b89f8d
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-02 23:43:53 +0530

    Learn rules of hooks in practice

--- src/App-v2.js
+++ src/App-v2.js
@@ -321,10 +321,6 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     Genre: genre,
   } = movie;
 
-  /* eslint-disable */
-  // if (imdbRating > 8) [isTop, setIsTop] = useState(true);
-  // if (imdbRating > 0) return <p>Greatest ever!</p>;
-
   function handleAdd() {
     const newWatchedMovie = {
       imdbID: selectedId,
