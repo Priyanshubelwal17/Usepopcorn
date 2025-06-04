8ddaabdf1d448de115b65052071d9f13e5bc11f4
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-04 17:10:43 +0530

    Add local storage for watch list

--- src/App.js
+++ src/App.js
@@ -56,10 +56,14 @@ const KEY = "34ffe78c";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState();
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
   const tempQuery = "interstellar";
 
   // useEffect(function () {
@@ -85,7 +89,16 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   function handeDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
