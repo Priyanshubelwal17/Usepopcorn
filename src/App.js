bda24b9e5082a989312590c15cf21e1e1f0ef6b0
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-05 10:46:25 +0530

    Focus input on mount using useEffect

--- src/App.js
+++ src/App.js
@@ -1,53 +1,6 @@
 import { use, useEffect, useState } from "react";
 import StarRating from "./StarRating.js";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -66,20 +19,6 @@ export default function App() {
   });
   const tempQuery = "interstellar";
 
-  // useEffect(function () {
-  //   console.log("After initial render");
-  // }, []);
-  // useEffect(function () {
-  //   console.log("After every render");
-  // });
-  // useEffect(
-  //   function () {
-  //     console.log("D");
-  //   },
-  //   [query]
-  // );
-  // console.log("During render");
-
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -89,8 +28,6 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
-
-    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   useEffect(
@@ -152,7 +89,7 @@ export default function App() {
   return (
     <>
       <NavBar>
-        <SearchBar query={query} setQuery={setQuery} />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
       {/* <Box element={<MovieList movies={movies} />} />
@@ -227,15 +164,15 @@ function Logo() {
   );
 }
 
-function NumResults({ movies }) {
-  return (
-    <p className="num-results">
-      Found <strong>{movies.length}</strong> results
-    </p>
+function Search({ query, setQuery }) {
+  useEffect(
+    function () {
+      const el = document.querySelector(".search");
+      console.log(el);
+      el.focus();
+    },
+    [query]
   );
-}
-
-function SearchBar({ query, setQuery }) {
   return (
     <input
       className="search"
@@ -247,6 +184,13 @@ function SearchBar({ query, setQuery }) {
   );
 }
 
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
 function Main({ children }) {
   return <main className="main">{children}</main>;
 }
@@ -347,8 +291,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   //   [imdbRating]
   // );
 
-  const isTop = imdbRating > 8;
-  console.log(isTop);
+  // const isTop = imdbRating > 8;
+  // console.log(isTop);
 
   // const [avgRating, setAvgRating] = useState(0);
 
@@ -375,7 +319,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       function callback(e) {
         if (e.code === "Escape") {
           onCloseMovie();
-          console.log(`CLOSING DUE TO ${e.code}`);
+          // console.log(`CLOSING DUE TO ${e.code}`);
         }
       }
 
@@ -410,7 +354,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
     return function () {
       document.title = "usePopcorn";
-      console.log(`Clean up effect for movie ${title}`);
+      // console.log(`Clean up effect for movie ${title}`);
     };
   }, [title]);
 
