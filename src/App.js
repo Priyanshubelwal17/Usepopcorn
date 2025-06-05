9cb5edbe55db22a35ade8aa422331cd001af277d
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-05 18:35:55 +0530

    Track user rating changes using refs

--- src/App.js
+++ src/App.js
@@ -278,6 +278,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+  let count = 0;
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+      if (userRating) count++;
+    },
+    [userRating, count]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -323,6 +334,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecision: countRef.current,
+      count: count,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
